a5a9a9d6aae3fadd18368c8f62e6b1d0ce5c99e3
Esioner <727240908@qq.com>
2017-11-27 17:19:08 +0800

    泛型接口

---
+++ src/Genericity/GenericityInterface.java
@@ -0,0 +1,19 @@
+package Genericity;
+
+public class GenericityInterface {
+	public static void main(String[] args) {
+		InfoInterface<String> info = null;
+		info = new InfoImpl<String>("张三");
+		System.out.println(info.getVar());
+	}
+}
+// 在接口上定义泛型
+interface InfoInterface<T> {public T getVar();}
+// 定义泛型的子类
+class InfoImpl<T> implements InfoInterface<String> {
+	private String var;
+	public InfoImpl(String var) {this.setVar(var);}
+	public void setVar(String var) {this.var = var;}
+	@Override
+	public String getVar() {return this.var;}
+}
